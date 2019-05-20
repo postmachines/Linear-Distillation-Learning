b4c4ee283322f8435bf1a6705a44c5eeaec13a80
Kuznetsov Igor <igorkuzentsov14@gmail.com>
2019-05-21 01:30:03 +0300

    Baselines code

--- did/experiments/baselines/models.py
+++ did/experiments/baselines/models.py
@@ -29,3 +29,37 @@ class MLP(nn.Module):
     def forward(self, x):
         x = x.view(-1, self.x_dim**2)
         return F.log_softmax(self.model(x), dim=-1)
+
+
+class LogReg(nn.Module):
+
+    def __init__(self, n_classes=10, x_dim=28):
+        super(LogReg, self).__init__()
+        self.model = nn.Linear(x_dim**2, n_classes)
+
+    def forward(self, x):
+        out = self.model(x)
+        return out
+
+
+class TinyCNN(nn.Module):
+    def __init__(self, n_classes=10):
+        super(TinyCNN, self).__init__()
+        self.layer1 = nn.Sequential(
+                nn.Conv2d(1, 16, kernel_size=5, stride=1, padding=2),
+                nn.BatchNorm2d(16),
+                nn.ReLU(),
+                nn.MaxPool2d(kernel_size=2, stride=2))
+        self.layer2 = nn.Sequential(
+                nn.Conv2d(16, 32, kernel_size=5, stride=1, padding=2),
+                nn.BatchNorm2d(32),
+                nn.ReLU(),
+                nn.MaxPool2d(kernel_size=2, stride=2))
+        self.fc = nn.Linear(7*7*32, n_classes)
+
+    def forward(self, x):
+        out = self.layer1(x)
+        out = self.layer2(out)
+        out = out.reshape(out.size(0), -1)
+        out = self.fc(out)
+        return out
